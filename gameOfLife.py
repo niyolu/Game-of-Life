995e5e3850085ffea85cf0f2236003d9b9369e1d
niyolu <nijolu@live.de>
2022-02-23 14:38:36 +0100

    Refactors and grid resizing

--- gameOfLife.py
+++ gameOfLife.py
@@ -2,69 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
+
 class GameOfLife:
-    def __init__(self, board):
-        self.board = np.array(board)
-        self.n, self.m = self.board.shape
-        
+    def __init__(self, grid, dim=None):
+        self.grid = GameOfLife.prepare_grid(grid, dim)
+        self.n, self.m = self.grid.shape
+
     def update(self):
         changes = []
-        
-        for row in range(self.n):
-            for col in range(self.m):
-                neighbour_count = self.get_neighbour_count(row, col)
-                state = self.board[row, col]
-                state_change = self.apply_rules(state, neighbour_count)
+
+        for row_idx, row in enumerate(self.grid):
+            for col_idx, cell_state in enumerate(row):
+                neighbour_count = self.get_neighbour_count(row_idx, col_idx)
+                state_change = self.apply_rules(cell_state, neighbour_count)
                 if state_change:
-                    changes.append((row, col))
-        
+                    changes.append((row_idx, col_idx))
+
         for row, col in changes:
             self.swap(row, col)
-            
+
     def is_alive(self, row, col):
-        if row in (-1, self.n) or col in (-1, self.m):
-            return 0
-        return self.board[row, col]
-                
-    
+        if row in range(self.n) and col in range(self.m):
+            return self.grid[row, col]
+        return 0
+
     def get_neighbour_count(self, row, col):
-        idxs = ((row + i, col + j) for i in (0, 1, -1) for j in (0, 1, -1) if not i == j == 0)
+        idxs = ((row + i, col + j)
+                for i in (0, 1, -1)
+                for j in (0, 1, -1)
+                if not i == j == 0)
         count = sum(self.is_alive(*idx) for idx in idxs)
         return count
-    
+
     def apply_rules(self, state, neighbour_count):
         alive = state == 1
         death = alive and not neighbour_count in (2, 3)
         birth = not alive and neighbour_count == 3
         return death or birth
-    
+
     def swap(self, row, col):
-        self.board[row][col] ^= 1
-        
-    def visualize(self):
-        plt.imshow(self.board, cmap="binary")
-        plt.show()
-        
-    def simulate(self, generation_decades=1):
+        self.grid[row][col] ^= 1
+
+    def visualize(self, X, Y, ax, gen):
         # matplot magic stolen from https://stackoverflow.com/a/55876734
+        ax.scatter(X[self.grid > 0], Y[self.grid > 0], color="k")
+        ax.grid(True, color="k")
+        ax.xaxis.set_major_locator(mticker.MultipleLocator())
+        ax.yaxis.set_major_locator(mticker.MultipleLocator())
+        ax.tick_params(size=0, length=0, labelleft=False,
+                       labelbottom=False)
+        ax.set(xlim=(0, self.m), ylim=(self.n, 0),
+               title=f'Generation {gen + 1}',
+               aspect="equal")
+
+    def simulate(self, generation_decades=1):
         ax_rows = 10 * generation_decades // 5
-                
-        Z = self.board
-
-        X, Y = np.meshgrid(np.arange(Z.shape[1])+.5, np.arange(Z.shape[0])+.5)
-
         fig, axes = plt.subplots(ax_rows, 5, figsize=(16, 8))
-        for i, ax in enumerate(axes.flat):
-            ax.scatter(X[Z > 0], Y[Z > 0], color="k")
-
-            ax.grid(True, color="k")
-            ax.xaxis.set_major_locator(mticker.MultipleLocator())
-            ax.yaxis.set_major_locator(mticker.MultipleLocator())
-            ax.tick_params(size=0, length=0, labelleft=False, labelbottom=False)
-            ax.set(xlim=(0, Z.shape[1]), ylim=(Z.shape[0], 0),
-                title=f'Generation {i+1}', aspect="equal")
-            
+        X, Y = np.meshgrid(
+            np.arange(self.m) + .5, np.arange(self.n) + .5)
+        for gen, ax in enumerate(axes.flat):
+            self.visualize(X, Y, ax, gen)
             self.update()
 
         plt.tight_layout()
         plt.show()
+
+    @classmethod
+    def prepare_grid(cls, grid, dim):
+        if not isinstance(grid, np.ndarray):
+            grid = np.array(grid)
+        if dim and dim != grid.shape:
+            grid = np.pad(grid, mode="constant", pad_width=(
+                (0, dim[0] - grid.shape[0]), (0, dim[1] - grid.shape[1])))
+        return grid
--- tests.py
+++ tests.py
@@ -1,84 +1,87 @@
+from re import M, X
 import numpy as np
-from gameOfLife import GameOfLife
-        
+from gameoflife import GameOfLife
+
+
 def TC_mock_interview(debug=False):
+    # https://www.youtube.com/watch?v=T9Y82JH4-pY&t=450s
+
     state0 = [
-        [0,1,0],
-        [0,0,1],
-        [1,1,1],
-        [0,0,0]
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
     ]
 
     state1 = [
-        [0,0,0],
-        [1,0,1],
-        [0,1,1],
-        [0,1,0]
+        [0, 0, 0],
+        [1, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0]
     ]
 
-
     gol = GameOfLife(state0)
+
     if debug:
         gol.visualize()
-        print(gol.board)
-    
+        print(gol.grid)
+
     assert gol.get_neighbour_count(0, 1) == 1
     assert gol.get_neighbour_count(0, 0) == 1
     assert gol.get_neighbour_count(2, 2) == 2
     assert gol.get_neighbour_count(3, 1) == 3
-        
+
     gol.update()
+
     if debug:
         gol.visualize()
-        print(gol.board)
-    assert np.array_equal(gol.board, np.array(state1))
-    
-    
-def simulate_beehive():
-    gol = GameOfLife([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        print(gol.grid)
 
-    gol.simulate(2)
-    
+    assert np.array_equal(gol.grid, np.array(state1))
+
+
+def simulate_beehive():
+    gol = GameOfLife([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    gol.simulate()
+
+
 def simulate_mock():
     gol = GameOfLife([
-        [0,1,0],
-        [0,0,1],
-        [1,1,1],
-        [0,0,0]
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
     ])
 
     gol.simulate()
-    
-def simulate_glider():
-    gol = GameOfLife([
-        [0,0,1,0,0,0,0,0,0,0,0,0],
-        [1,0,1,0,0,0,0,0,0,0,0,0],
-        [0,1,1,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0]
-    ])
 
-    gol.simulate(2)
+def simulate_glider():
+
+    glider = [[0, 0, 1],
+           [1, 0, 1],
+           [0, 1, 1]]
     
+    gol = GameOfLife(glider, (12, 12))
+
+    gol.simulate(3)
+
 def simulate_blinker():
     gol = GameOfLife([
-        [0,0,0,0,0],
-        [0,0,1,0,0],
-        [0,0,1,0,0],
-        [0,0,1,0,0],
-        [0,0,0,0,0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
     ])
 
     gol.simulate()
@@ -86,3 +89,4 @@ def simulate_blinker():
 if __name__ == "__main__":
     TC_mock_interview()
     simulate_glider()
+   
